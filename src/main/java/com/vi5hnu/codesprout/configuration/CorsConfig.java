63b4e6a7de27592619f1da98cc79103bcddc814c
vishnu kumar <kumarvishnu1619@gmail.com>
2025-12-23 08:41:39 +0530

    updated cors

--- src/main/java/com/vi5hnu/codesprout/configuration/CorsConfig.java
+++ src/main/java/com/vi5hnu/codesprout/configuration/CorsConfig.java
@@ -21,8 +21,8 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration configuration=new CorsConfiguration();
 
-        if(Arrays.asList(environment.getActiveProfiles()).contains("dev")) configuration.setAllowedOrigins(List.of("http://localhost:4200"));
-        else configuration.setAllowedOrigins(List.of("https://code-sprout.laxmi.solutions"));
+        if(Arrays.asList(environment.getActiveProfiles()).contains("dev")) configuration.setAllowedOrigins(List.of("http://localhost:3000","http://localhost:4200","http://10.120.240.148:4200"));
+        else configuration.setAllowedOrigins(List.of("https://code-sprout.laxmi.solutions","https://the-code-sprout.netlify.app/"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true); // Important if using authentication
